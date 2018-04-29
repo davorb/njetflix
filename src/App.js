c26d3c6c9711eb3725245cafc86a61b2ee8641b1
Davor Babić <davor@davor.se>
2018-04-29 12:43:11 +0200

    add react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Menu from './Menu';
 import Genres from './Genres';
@@ -6,12 +7,18 @@ import Movies from './Movies';
 
 function App() {
   return (
-    <div className="app">
-      <Menu />
-      <Genres />
-      <Movies />
-    </div>
+    <Router>
+      <Route exact path="/" component={Main} />
+    </Router>
   );
+}
+
+function Main() {
+  return (<React.Fragment>
+    <Menu />
+    <Genres />
+    <Movies />
+  </React.Fragment>);
 }
 
 export default App;
